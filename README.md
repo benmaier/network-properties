0d7dac0a6300db4464f76bb55794d520617dcfec
Benjamin Maier <benjaminfrankmaier@gmail.com>
2016-01-25 15:07:13 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Modular Hierarchical Random Networks
+
+## Install 
+
+    $ sudo python setup.py install
+
+## Example
+
+    $ python example.py
---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name='networkprops',
+      version='0.1',
+      description='Compute a bunch of network properties for a given graph',
+      url='https://github.com/benmaier/Get-Network-Properties',
+      author='Benjamin F. Maier',
+      author_email='bfmaier@physik.hu-berlin.de',
+      license='MIT',
+      packages=['networkprops'],
+      install_requires=[
+          'numpy',
+          'scipy',
+          'networkx',
+          'python-louvain',
+      ],
+      zip_safe=False)
